9a7b8e596dcfd7b8de3cbd9be27dd6d798784e71
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-02 22:46:19 +0100

    Add inspec tests
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

---
+++ controls/flex_exists.rb
@@ -0,0 +1,22 @@
+title 'Tests to confirm flex exists'
+
+plan_origin = ENV['HAB_ORIGIN']
+plan_name = input('plan_name', value: 'flex')
+ 
+control 'core-plans-flex-exists' do
+  impact 1.0
+  title 'Ensure flex exists'
+  desc '
+  '
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+
+  command_relative_path = input('command_relative_path', value: '/bin/flex')
+  command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
+  describe file(command_full_path) do
+    it { should exist }
+  end
+end
